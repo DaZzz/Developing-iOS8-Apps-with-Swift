0fa15db2949c44d77657e724c66b180fc7f71fa2
Tikhon Belousko <dazmail01@gmail.com>
2015-02-07 19:15:41 +0300

    Initial version of Calculator.

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Tikhon Belousko on 04/02/15.
+//  Copyright (c) 2015 Tikhon Belousko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var userIsInTheMiddleOfTypingANumber = false
+    var brain = CalculatorBrain()
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        
+        set {
+            display.text = "\(newValue)"
+        }
+    }
+    
+    @IBOutlet weak var display: UILabel!
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        if let operation = sender.currentTitle {
+            if let result = brain.performOperation(operation) {
+                displayValue = result
+            } else {
+                displayValue = 0
+            }
+        }
+    }
+    
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        if let result = brain.pushOperand(displayValue) {
+            displayValue = result
+        } else {
+            displayValue = 0
+        }
+    }
+}
+
